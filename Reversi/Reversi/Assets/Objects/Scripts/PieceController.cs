8cb053f1aae7a60da858a2d4cbb21bc550475e3f
JamieSampson08 <jsampson2117@gmail.com>
2020-04-18 00:10:03 -0500

    added characterand animations

---
+++ Reversi/Reversi/Assets/Objects/Scripts/PieceController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceController : MonoBehaviour
+{
+    public GameObject boardPiece;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        for (int i = 0; i < 54; i++)
+        {
+            GameObject temp = GameObject.Instantiate<GameObject>(boardPiece);
+            temp.transform.position = new Vector3(2, 2, 2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
